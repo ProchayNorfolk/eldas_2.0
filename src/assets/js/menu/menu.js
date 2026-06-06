a15437b49b5a24061b75de21ddb0952cf47cc0d4
Prochay Norfolk <prochaynorfolk@gmail.com>
2026-06-06 23:34:37 +0200

    Active menu and SPA routing step 1

--- src/assets/js/menu/menu.js
+++ src/assets/js/menu/menu.js
@@ -1,13 +1,48 @@
-export function initMenu() {
+export function initMenu(
+  
+) {
+  /* ==================== header scroll ==================== */
+
   const header = document.querySelector('.header');
 
-  if (!header) return;
+  if (header) {
+    window.addEventListener('scroll', () => {
+      header.classList.toggle('scrolled', window.scrollY > 50);
+    });
+  }
 
-  window.addEventListener('scroll', () => {
-    header.classList.toggle('scrolled', window.scrollY > 50);
+  /* ==================== active link menu ==================== */
+
+  const links = document.querySelectorAll('.nav-link');
+
+  const currentPath =
+    window.location.pathname
+      .replace('/eldas_2.0', '')
+      .replace(/\/$/, '') || '/';
+
+  links.forEach(link => {
+    const linkPath = link
+    .getAttribute('href')
+    .replace('/eldas_2.0', '')
+
+    if (linkPath === currentPath) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
   });
 }
 
-/* ==================== mobile menu ==================== */
+document.addEventListener('click', e => {
+  const link = e.target.closest('[data-link]')
 
-/* ================== end mobile menu ================== */
+  if (!link) return
+
+  e.preventDefault()
+
+  const url = link.getAttribute('href')
+
+  history.pushState({}, '', url)
+
+  window.location.reload()
+})
